1beba107e65e531f7ddbfaa1c5b4872ada008934
Ajinkya Bhobad <ajinkya.bhobad@ixiasolutions.com>
2018-10-25 10:28:26 +0530

    Uploading From Eclipse to GitHub

---
+++ src/main/java/Email/EmailTest/CommonEmails.java
@@ -0,0 +1,29 @@
+package Email.EmailTest;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+public class CommonEmails {
+
+	public static void main(String[] args) throws EmailException 
+	{
+		System.out.println("Started");
+		
+		Email email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(465);
+		email.setAuthenticator(new DefaultAuthenticator("automation2018.QA@gmail.com", "automation@2018"));
+		email.setSSLOnConnect(true);
+		email.setFrom("ajinkyabhobad.t@gmail.com");
+		email.setSubject("TestMail");
+		email.setMsg("This is a test mail ... :-)");
+		email.addTo("foo@bar.com");
+		email.send();	
+		
+		System.out.println("End");
+
+	}
+
+}
---
+++ src/test/java/Email/EmailTest/EmailTests.java
@@ -0,0 +1,104 @@
+package Email.EmailTest;
+
+import javax.mail.Message;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class EmailTests 
+{
+	  public static WebDriver driver;	
+	  private static EmailUtils emailUtils;
+
+	  @BeforeClass
+	  public static void connectToEmail()  {
+	    try {
+	    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\ajinkya.bhobad\\Desktop\\jars\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			//driver.get("http://www.google.com");
+	    	driver.get("https://webmail.ixiasolutions.com/");
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//input[@id='rcmloginuser']")).sendKeys("ajinkya.bhobad@ixiasolutions.com");
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//input[@id='rcmloginpwd']")).sendKeys("Ixia@12345");
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//input[@id='rcmloginsubmit']")).click(); 
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//a[contains(text(),'Compose')]")).click(); 
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//*[@id=\"_to\"]")).sendKeys("automation2018.QA@gmail.com");
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//*[@id='composebody']")).sendKeys("Test Automation Mail"); 
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//*[@id='compose-subject']")).sendKeys("Test Automation Mail"); 
+	    	Thread.sleep(3000);
+	    	driver.findElement(By.xpath("//*[@id=\"rcmbtn107\"]")).click();   
+	    
+	      emailUtils = new EmailUtils("automation2018.QA@gmail.com", "automation@2018", "smtp.gmail.com", EmailUtils.EmailFolder.INBOX);
+	    } 
+	    catch (Exception e) {
+	      e.printStackTrace();
+	      Assert.fail(e.getMessage());
+	    }
+	  }
+	  
+ /* @Test
+  public void testVerificationCode() {
+    try {
+      //TODO: Execute actions to send verification code to email
+ 
+      String verificationCode = emailUtils.getAuthorizationCode();
+ 
+      //TODO: Enter verification code on screen and submit
+ 
+      //TODO: add assertions
+ 
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    }
+  }*/
+  
+  @Test
+  public void testTextContained() {
+    try{
+    Message email = emailUtils.getMessagesBySubject("Test Automation Mail", true, 5)[0];
+    Assert.assertTrue("Approval message is not in email", emailUtils.isTextInMessage(email, "Test Automation Mail"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    }
+  }
+  
+ /* @Test
+  public void testLink() {
+    
+    //TODO: apply for a loan using criteria that will result in the application being rejected
+    
+    try{
+      Message email = emailUtils.getMessagesBySubject("Test Automation Mail", true, 5)[0];
+      String link = emailUtils.getUrlsFromMessage(email, "Click here to view the reason").get(0);
+      
+      driver.get(link);
+      
+      //TODO: continue testing
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail(e.getMessage());
+    }
+  }*/
+  
+  @AfterClass
+  public void aftermethod()
+  {
+	  driver.close();
+  }
+
+}
